e6e66b55c615ea17e859dc7729af4d4bb9abd3bf
xurramovasliddin534 <xurramovasliddin534@gmail.com>
2026-01-17 17:37:35 +0500

    pg Admin yangi data basa bilan ishlash

--- books/models.py
+++ books/models.py
@@ -1,37 +1,76 @@
+# from django.db import models
+
+# from django.contrib.auth.models import User
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
+#     isbn = models.CharField(max_length=17)
+
+# def __str__(self):
+#         return self.title 
+# # nomini chiqarish
+
+# class Author(models.Model):
+#       first_name = models.CharField(max_length=200)
+#       last_name = models.CharField(max_length=200)
+#       email = models.EmailField()
+#       bio = models.TextField()
+      
+#       def __str__(self):
+#             return f'{self.first_name} {self.last_name}'
+# class BookAuthor(models.Model):
+#       book = models.ForeignKey(book, on_delete=models.CASCADE)   
+#       author = models.ForeignKey(author, on_delete=models.CASCADE)
+
+#       def __str__(self):
+#             f"{self.book.title} by {self.author.first_name} {self.author.last_name}"
+
+
+# class BookReview(models.Model):
+#       user = models.ForeignKey(User, on_delete=models.CASCADE)
+#       book = models.ForeignKey(book, on_delete=models.CASCADE)
+#       commit = models.TextField()
+#       stars_given =models.ImageField()
+
+#       def __str__(self):
+#             return f"{self.stars_given} star for {self.book.title} by {self.user.username}"
 from django.db import models
 
 from django.contrib.auth.models import User
 
+from django.core.validators import MinValueValidator, MaxValueValidator
 class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     isbn = models.CharField(max_length=17)
-
-def __str__(self):
+    
+    def __str__(self):
         return self.title 
-# nomini chiqarish
 
 class Author(models.Model):
-      first_name = models.CharField(max_length=200)
-      last_name = models.CharField(max_length=200)
-      email = models.EmailField()
-      bio = models.TextField()
-      
-      def __str__(self):
-            return f'{self.first_name} {self.last_name}'
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
+    bio = models.TextField()
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class BookAuthor(models.Model):
-      book = models.ForeignKey(book, on_delete=models.CASCADE)   
-      author = models.ForeignKey(author, on_delete=models.CASCADE)
-
-      def __str__(self):
-            f"{self.book.title} by {self.author.first_name} {self.author.last_name}"
-
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)   # TO'G'RILANDI: book → Book
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # TO'G'RILANDI: author → Author
+    
+    def __str__(self):
+        return f"{self.book.title} by {self.author.first_name} {self.author.last_name}"  # TO'G'RILANDI: return qo'shildi
 
 class BookReview(models.Model):
-      user = models.ForeignKey(User, on_delete=models.CASCADE)
-      book = models.ForeignKey(book, on_delete=models.CASCADE)
-      commit = models.TextField()
-      stars_given =models.ImageField()
-
-      def __str__(self):
-            return f"{self.stars_given} star for {self.book.title} by {self.user.username}"
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)  # TO'G'RILANDI: book → Book
+    comment = models.TextField()  # TO'G'RILANDI: commit → comment (odatdagi nomlanish)
+    stars_given = models.IntegerField(
+        validators =[MinValueValidator(1), MaxValueValidator((5))] 
+    )  # TO'G'RILANDI: ImageField → IntegerField
+    
+    def __str__(self):
+        return f"{self.stars_given} star for {self.book.title} by {self.user.username}"
--- config/settings.py
+++ config/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'goodread',
+        'HOST': 'localhost',
+        'POST': '5432',
+        'USER': 'Ali',
+        'PASSWORD': '6060',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
