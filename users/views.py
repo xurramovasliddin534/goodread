305ead8ee2c54b2cbbab664e1fd6eda0e3f7ea0c
xurramovasliddin534 <xurramovasliddin534@gmail.com>
2026-01-22 12:28:52 +0500

    userga form yaratildi

--- users/views.py
+++ users/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
-
+from django.contrib.auth.models import User
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'register.html')
+        return render(request, 'register.html')   
+    def post(self, request):
+        
+        username =request.POST['username']
+        first_name =request.POST['first_name']
+        last_name =request.POST['last_name']
+        email =request.POST['email']
+        password =request.POST['password']
+
+        user = User.objects.create(
+            username = username,
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            password = password,
+        )
+        user.save()
+        return redirect('user:login')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    
